5c1658cf129a2174b4366c193e5e20eaf186062c
vlimonoff <bogachevvova@gmail.com>
2024-06-20 02:26:09 +0300

    fix nginx

--- backend/netrav/settings.py
+++ backend/netrav/settings.py
@@ -146,9 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = '/app/media/'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
